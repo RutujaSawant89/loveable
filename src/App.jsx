73229da650fbb9557ff96dc1f94dae6bee25f6b7
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2025-08-30 20:28:45 +0530

    updated imports for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./screens/Landing";
-/* import Building from "./screens/Building";
-import Template from "./screens/Template"; */
+import Building from "./screens/Building";
+import Template from "./screens/Template";
 function App() {
   return (
     <BrowserRouter>
