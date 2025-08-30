ff7389055a63dbebcbba1dcb54691d2e6425db4d
RUTUJA SAWANT <rutujasawant8905@gmail.com>
2025-08-30 19:35:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./screens/Landing";
-import Building from "./screens/Building";
-import Template from "./screens/Template";
+/* import Building from "./screens/Building";
+import Template from "./screens/Template"; */
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/build" element={<Building />} />
-        <Route path="/templ" element={<Template />} />
+       {/*  <Route path="/build" element={<Building />} />
+        <Route path="/templ" element={<Template />} /> */}
       </Routes>
     </BrowserRouter>
   );
